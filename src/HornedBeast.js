5f04e0e4a57a381ab3a73e8dcef8baead4a019c8
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-05 19:39:35 -0400

    adding horned animal picture

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -5,9 +5,9 @@ class HornedBeast extends React.Component {
   render() {
     return(
      <>
-       <h2>Moose: Horned Animal</h2>
+       <h2>Horned Animals</h2>
        <img src={this.props.img_url} alt={this.props.alt} title={this.props.title}></img>
-       <p> This is a horned animal. A Moose</p>
+       <p> This is a horned animal</p>
      </> 
     )
   }
